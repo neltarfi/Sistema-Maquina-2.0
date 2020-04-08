0838da78572ab93370718efa244fe7f6daf4c674
Nelson Tarnoschi Filho <nelson759734@gmail.com>
2020-04-08 16:25:52 -0300

    Muda Lote4.1

--- mudalote.pas
+++ mudalote.pas
@@ -14,12 +14,18 @@ type
 
   TfrmMudaLote = class(TForm)
     btSeleciona: TButton;
+    btSalvar: TButton;
+    btSair: TButton;
     dsLoteDestino: TDataSource;
     dsLoteOrigem: TDataSource;
     dsGridOrigem: TDataSource;
     DBGrid1: TDBGrid;
     dblOrigem: TDBLookupComboBox;
-    DBLookupComboBox2: TDBLookupComboBox;
+    dblDestino: TDBLookupComboBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    qrDestinoCOD_ROMANEIO: TLongintField;
+    qrDestinoLOTECOCO: TLongintField;
     qrGridOrigemCOD_ROMANEIO: TLongintField;
     qrGridOrigemDATA: TDateField;
     qrGridOrigemLOTECOCO: TLongintField;
@@ -32,11 +38,15 @@ type
     qrGridOrigem: TSQLQuery;
     sgDestino: TStringGrid;
     qrLoteDestino: TSQLQuery;
+    qrDestino: TSQLQuery;
+    procedure btSairClick(Sender: TObject);
+    procedure btSalvarClick(Sender: TObject);
     procedure btSelecionaClick(Sender: TObject);
-    procedure DBLookupComboBox2Select(Sender: TObject);
+    procedure dblDestinoSelect(Sender: TObject);
     procedure dblOrigemSelect(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormShow(Sender: TObject);
+    procedure LimpaGrid;
   private
 
   public
@@ -48,43 +58,71 @@ var
   Coluna, Linha:integer;
 
 implementation
-
+    uses Module1;
 {$R *.lfm}
 
 { TfrmMudaLote }
 
 
 procedure TfrmMudaLote.dblOrigemSelect(Sender: TObject);
-Var i,j:integer;
 begin
      qrGridOrigem.Close;
      qrGridOrigem.ParamByName('IDOrigem').Value:=dblOrigem.KeyValue;
      qrGridOrigem.Open;
-     if Linha>0 then
-     for i:=Linha to 3 do
-         for j:=Coluna to Linha do
-         sgDestino.Cells[j,i]:='';
-     Coluna:=1;
-     Linha:=1;
+     LimpaGrid;
 end;
 
 procedure TfrmMudaLote.btSelecionaClick(Sender: TObject);
+var i:integer;
+    SelecionaTrue:boolean;
 begin
-     sgDestino.Cells[1,Linha]:=qrGridOrigem.FieldByName('Data').Text;
-     sgDestino.Cells[2,Linha]:=qrGridOrigem.FieldByName('Cod_Romaneio').Text;
-     sgDestino.Cells[3,Linha]:=qrGridOrigem.FieldByName('Nome').Text;
-     Linha:=Linha+1;
+     SelecionaTrue:=True;
+     for i:=1 to Linha do
+        if qrGridOrigem.FieldByName('Cod_Romaneio').Text = sgDestino.Cells[2,i] then
+           SelecionaTrue:=False;
+     if (dblDestino.KeyValue < 1) or (dblOrigem.KeyValue = dblDestino.KeyValue) then
+        SelecionaTrue:=False;
+     if SelecionaTrue then
+     Begin
+          Linha:=Linha+1;
+          sgDestino.Cells[1,Linha]:=qrGridOrigem.FieldByName('Data').Text;
+          sgDestino.Cells[2,Linha]:=qrGridOrigem.FieldByName('Cod_Romaneio').Text;
+          sgDestino.Cells[3,Linha]:=qrGridOrigem.FieldByName('Nome').Text;
+     end;
 end;
 
-procedure TfrmMudaLote.DBLookupComboBox2Select(Sender: TObject);
-var i,j:integer;
+procedure TfrmMudaLote.btSairClick(Sender: TObject);
 begin
-     if Linha>0 then
-     for i:=Linha to 3 do
-         for j:=Coluna to Linha do
-         sgDestino.Cells[j,i]:='';
-     Coluna:=1;
-     Linha:=1;
+     frmMudaLote.Close;
+end;
+
+procedure TfrmMudaLote.btSalvarClick(Sender: TObject);
+var i:integer;
+begin
+     if linha > 0 then
+     begin
+         for i:=1 to Linha do
+             begin
+                  qrDestino.Close;
+                  qrDestino.ParamByName('IDCodigo').Value:=strToint(sgDestino.Cells[2,i]);
+                  qrDestino.Open;
+                  qrDestino.Edit;
+                  qrDestino.FieldByName('LoteCoco').Value:=dblDestino.KeyValue;
+                  qrDestino.ApplyUpdates;
+             end;
+         DataModule1.SQLTMaquina.CommitRetaining;
+         dblOrigem.KeyValue:=0;
+         dblDestino.KeyValue:=0;
+         LimpaGrid;
+         qrGridOrigem.Close;
+         qrGridOrigem.ParamByName('IDOrigem').Value:=0;
+         qrGridOrigem.Open;
+     end;
+end;
+
+procedure TfrmMudaLote.dblDestinoSelect(Sender: TObject);
+begin
+     LimpaGrid;
 end;
 
 procedure TfrmMudaLote.FormClose(Sender: TObject; var CloseAction: TCloseAction
@@ -92,13 +130,23 @@ procedure TfrmMudaLote.FormClose(Sender: TObject; var CloseAction: TCloseAction
 begin
      qrLoteOrigem.Close;
      qrGridOrigem.Close;
+     qrDestino.Close;
 end;
 
 procedure TfrmMudaLote.FormShow(Sender: TObject);
 begin
      qrLoteOrigem.Open;
      qrGridOrigem.Open;
-     Coluna:=0;
+     LimpaGrid;
+end;
+procedure TfrmMudaLote.LimpaGrid;
+var i,j:integer;
+begin
+     if Linha>0 then
+     for i:=1 to Linha do
+         for j:=1 to Coluna do
+         sgDestino.Cells[j,i]:='';
+     Coluna:=3;
      Linha:=0;
 end;
 
